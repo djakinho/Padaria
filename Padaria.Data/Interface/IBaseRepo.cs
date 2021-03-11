281fa2e33fb24a6d859a7908738bf4763d16b868
Djakson Cleber <djakson.filho@gmail.com>
2021-03-10 21:53:26 -0300

    Add project files.

---
+++ Padaria.Data/Interface/IBaseRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Padaria.Domain;
+
+
+namespace Padaria.Data.Interface
+{
+    public interface IBaseRepo<T> where T : class, IEntity
+    {
+
+    }
+}
