0ba8830510c3cb3f2d84ff29d80dfa44284dae7f
Djakson Cleber <djakson.filho@gmail.com>
2021-03-13 16:23:36 -0300

    Maps and Injects TipoProducao_UnidadeMedida

---
+++ Padaria.Data/Migrations/20210313182949_Adds TipoProducao_UnidadeMedida.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Padaria.Data.Migrations
+{
+    public partial class AddsTipoProducao_UnidadeMedida : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoProducao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoProducao", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnidadeMedida",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Unidade = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadeMedida", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoProducao");
+
+            migrationBuilder.DropTable(
+                name: "UnidadeMedida");
+        }
+    }
+}
