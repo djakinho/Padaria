a993f4aff48f4bf10155f6e19a19b99a9d28f773
Marcelo Ludovico <maludovico@msn.com>
2021-03-13 11:45:49 -0300

    interface e repositorio produto

---
+++ Padaria.Data/Interface/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using Padaria.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Padaria.Data.Interface
+{
+    interface IProdutoRepository : IBaseRepository<Produto>
+    {
+    }
+}
