42cd1c2fa714e19abe960d322e5be9ba78f86ca2
waleriamf <waleriamf@gmail.com>
2021-03-19 11:02:13 -0300

    add realizarvenda

---
+++ Padaria.Data/Map/RealizarVendaProdutoMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Padaria.Domain.Model;
+
+namespace Padaria.Data.Map
+{
+    public class RealizarVendaProdutoMap : IEntityTypeConfiguration<RealizarVendaProduto>
+    {
+        public void Configure(EntityTypeBuilder<RealizarVendaProduto> builder)
+        {
+            builder.ToTable("RealizarVendaProduto");
+
+            builder.HasKey(x => new { x.IdRealizarVenda, x.IdProduto });
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn();
+
+            builder.HasOne(x => x.RealizarVenda)
+                .WithMany(x => x.RealizarVendaProduto)
+                .HasForeignKey(x => x.RealizarVenda)
+                .IsRequired();
+
+            builder.HasOne(x => x.Produto)
+                .WithMany(x => x.RealizarVendaProduto)
+                .HasForeignKey(x => x.IdProduto)
+                .IsRequired();
+        }
+    }
+}
